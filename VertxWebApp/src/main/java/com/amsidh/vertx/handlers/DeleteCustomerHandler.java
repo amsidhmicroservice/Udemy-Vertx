51282293c1fe51bc7495c190c8a5e473665e5000
Amsidh Lokhande <amsidhmicroservice@gmail.com>
2023-06-24 01:02:25 +0530

    Database get all, get post, delete operation

--- VertxWebApp/src/main/java/com/amsidh/vertx/handlers/DeleteCustomerHandler.java
+++ VertxWebApp/src/main/java/com/amsidh/vertx/handlers/DeleteCustomerHandler.java
@@ -3,16 +3,22 @@ package com.amsidh.vertx.handlers;
 import com.amsidh.vertx.model.Customer;
 import com.amsidh.vertx.service.CustomerService;
 import com.amsidh.vertx.service.impl.CustomerServiceImpl;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.Handler;
+import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.sqlclient.Pool;
+import io.vertx.sqlclient.templates.SqlTemplate;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Collections;
 import java.util.Optional;
 
 @Slf4j
 public class DeleteCustomerHandler implements Handler<RoutingContext> {
+    public static final String DELETE_CUSTOMER_SQL_QUERY = "DELETE FROM CUSTOMER.CUSTOMER WHERE id=#{customerId}";
     CustomerService customerService = new CustomerServiceImpl();
     Pool pool;
 
@@ -22,7 +28,28 @@ public class DeleteCustomerHandler implements Handler<RoutingContext> {
 
     @Override
     public void handle(RoutingContext routingContext) {
-        String id = routingContext.pathParam("id");
+        String customerId = routingContext.pathParam("id");
+        //deleteFromInmemoryCollection(routingContext, customerId);
+        SqlTemplate.forUpdate( pool, DELETE_CUSTOMER_SQL_QUERY)
+                .execute(Collections.singletonMap("customerId", Long.parseLong(customerId)))
+                .onFailure(error -> {
+                    log.error("Delete customer from database query failed with reason {}", error);
+                    routingContext.response()
+                            .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
+                            .setStatusCode(HttpResponseStatus.INTERNAL_SERVER_ERROR.code())
+                            .end(new JsonObject().put("message", error.getMessage()).toBuffer());
+                })
+                .onSuccess(customers -> {
+                        JsonObject response = new JsonObject().put("message", "Customer deleted successfully");
+                        log.info("Request {} and Response {} ", routingContext.normalizedPath(), response.encode());
+                        routingContext.response()
+                                .setStatusCode(HttpResponseStatus.OK.code())
+                                .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
+                                .end(response.encodePrettily());
+                });
+    }
+
+    private void deleteFromInmemoryCollection(RoutingContext routingContext, String id) {
         Optional<Customer> optionalCustomer = customerService.deleteCustomerById(Long.parseLong(id));
         JsonObject response = new JsonObject();
         if (optionalCustomer.isPresent()) {
--- VertxWebApp/src/main/java/com/amsidh/vertx/handlers/PostCustomerHandler.java
+++ VertxWebApp/src/main/java/com/amsidh/vertx/handlers/PostCustomerHandler.java
@@ -4,18 +4,23 @@ import com.amsidh.vertx.model.Customer;
 import com.amsidh.vertx.service.CustomerService;
 import com.amsidh.vertx.service.impl.CustomerServiceImpl;
 import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.sqlclient.Pool;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.templates.SqlTemplate;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Collections;
 import java.util.Optional;
 
 @Slf4j
 public class PostCustomerHandler implements Handler<RoutingContext> {
+    public static final String SAVE_CUSTOMER_SQL_QUERY = "INSERT INTO CUSTOMER.CUSTOMER(name) VALUES (#{customerName})";
     CustomerService customerService = new CustomerServiceImpl();
     Pool pool;
 
@@ -27,6 +32,32 @@ public class PostCustomerHandler implements Handler<RoutingContext> {
     public void handle(RoutingContext routingContext) {
         JsonObject requestBody = routingContext.getBodyAsJson();
         Customer customer = requestBody.mapTo(Customer.class);
+        SqlTemplate.forUpdate(pool, SAVE_CUSTOMER_SQL_QUERY)
+                .mapTo(Customer.class)
+                .execute(Collections.singletonMap("customerName", customer.getName()))
+                .onFailure(error -> {
+                    log.error("Save customer database query failed with reason {}", error);
+                    routingContext.response()
+                            .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
+                            .setStatusCode(HttpResponseStatus.INTERNAL_SERVER_ERROR.code())
+                            .end(new JsonObject().put("message", error.getMessage()).toBuffer());
+                })
+                .onSuccess(customers -> {
+                    if(!routingContext.response().ended()) {
+                        JsonObject response = new JsonObject().put("message", "Customer saved successfully");
+                        log.info("Request {} and Response {} ", routingContext.normalizedPath(), response.encode());
+                        routingContext.response()
+                                .setStatusCode(HttpResponseStatus.CREATED.code())
+                                .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
+                                .end(response.encodePrettily());
+                    }
+                });
+
+
+        //saveCustomerInMemoryCollection(routingContext, customer);
+    }
+
+    private void saveCustomerInMemoryCollection(RoutingContext routingContext, Customer customer) {
         Optional<Customer> optionalCustomer = customerService.saveCustomer(customer);
         JsonObject response;
         if (optionalCustomer.isPresent()) {
